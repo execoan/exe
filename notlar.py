8f911397e2743bc1eedba5e22b0443c559d8054e
execoan <55671984+execoan@users.noreply.github.com>
2020-05-09 15:41:54 +0300

    Update notlar.py

--- notlar.py
+++ notlar.py
@@ -1,2 +1,11 @@
  np.random.choice(0, 5, 3, p=[0.1, 0, 0.3, 0.6, 0])
  #  0 ile 5 arası p olasılığına göre 3 tane sayı. 
+ 
+dizi1 = ['a','b','c','d']
+olasilik = [0.2,0.4,0.4,0]
+print(np.random.choice(dizi1,4,p=olasilik))
+#dizi içinden olasilikla, olasilik toplaminin 1 olduguna dikkat et. 
+
+Constructer ile İç içe sözlük
+child = dict(child1=dict(name='hasan',yas=21),child2=dict(name='ebru',yas=25))
+print(child, child.keys(), child.get('child1'))
