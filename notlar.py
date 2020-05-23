df63c178a38954cc04d5660a9a6b0b7ad24e33ae
execoan <55671984+execoan@users.noreply.github.com>
2020-05-23 19:18:37 +0300

    Update notlar.py

--- notlar.py
+++ notlar.py
@@ -1,14 +1,17 @@
 #  0 ile 5 arası p olasılığına göre 3 tane sayı. 
 np.random.choice(0, 5, 3, p=[0.1, 0, 0.3, 0.6, 0])
+###################################################################
 
 #dizi içinden olasilikla, olasilik toplaminin 1 olduguna dikkat et. 
 dizi1 = ['a','b','c','d']
 olasilik = [0.2,0.4,0.4,0]
 print(np.random.choice(dizi1,4,p=olasilik))
+###################################################################
 
 #Constructer ile İç içe sözlük
 child = dict(child1=dict(name='hasan',yas=21),child2=dict(name='ebru',yas=25))
 print(child, child.keys(), child.get('child1'))
+###################################################################
 
 #Decoratorler
 #Decoratorleri özellikle loglama, error yakalama ve süre hesabı için kullanmak mantıklı.
@@ -21,10 +24,12 @@ class metin():
         return 'deneme'
 kelime = metin()
 print(kelime.yaz)
+###################################################################
 
 #Einstein-Summation
 #numpy.einsum(subscripts, *operands, out=None, dtype=None, order='K', casting='safe', optimize=False)
 
+###################################################################
 
 import numpy as np
 np.random.seed(69)
@@ -37,6 +42,7 @@ print(len(chain2))
 #rastgele binomial dağılım üretip sonra onun içindeki elemanlara tek tek erişmek,
 #ayrıca bu kodla liste içinde for döngüsü kullanımıda örneklendi.
 
+###################################################################
 
 x, y, z = 0, 1, 0
 
@@ -65,4 +71,14 @@ def buyut(yazi):
 yeni_liste = list(map(buyut,['test1','test2']))
 
 print(yeni_liste)
+###################################################################
+list1=[1,2,3]
+
+list3=[1,2,3]
+
+list2 = list1
+
+print(list2==list1,list1==list3)
+print(list2 is list1, list1 is list3)
+#is objelerin aynı olup olmadığını karşılastırırken == içeriğine bakıyor.
 ###################################################################
